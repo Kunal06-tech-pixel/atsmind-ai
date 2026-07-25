10e1002bbd3ff101994be2bfec0ea045cc528dcc
Kunal <kunaldaskumar3@gmail.com>
2026-07-25 16:21:49 +0530

    chore: remove frontend directory (moved to separate repo)

--- frontend/src/services/authService.js
+++
@@ -1,6 +0,0 @@
-import api from "./axios";
-
-export const signupUser = (data) => api.post("/api/auth/signup", data);
-export const loginUser = (data) => api.post("/api/auth/login", data);
-export const refreshSession = () => api.post("/api/auth/refresh");
-
